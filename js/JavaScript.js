da3d9921c80acdee3cfa725b7cd577c64f41163f
Andreas5524 <81920286+Andreas5524@users.noreply.github.com>
2021-08-24 20:03:15 +0200

    Color locking feature, palette in URL + more
    Color locking
    Color palette in URL
    CSS loading animation
    More responsive
    SEO and accessibility improvements

---
+++ js/JavaScript.js
@@ -0,0 +1,17 @@
+﻿window.clipboard = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text)
+        clipboardChanged();
+    }
+}
+
+document.oncopy = clipboardChanged;
+window.onfocus = windowFocus;
+
+function clipboardChanged() {
+    DotNet.invokeMethodAsync("ColorPaletteGenerator", "InvokeOnClipboardChanged");
+}
+
+function windowFocus() {
+    DotNet.invokeMethodAsync("ColorPaletteGenerator", "InvokeOnWindowFocus");
+}
